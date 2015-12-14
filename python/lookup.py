ef54d6bff1173b5d6859e42e75b8a7f61d10ec73
struct78 <david@struct.com.au>
2015-12-14 12:23:36 +1100

    Modified to take advantage of multiprocessing

--- python/lookup.py
+++ python/lookup.py
@@ -1,35 +1,41 @@
 import os
 import urllib
 import time
+from multiprocessing import Process, Pool
 
 api_key = os.environ['DICTIONARY_API_KEY']
 alpha_numeric = ['1','2','3','4','5','6','7','8','9','0','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
 dictionaries = ['dictionaries/new-additions.txt', 'dictionaries/english-words.txt', '/usr/share/dict/words']
+directories = ['data','parsed']
 definition_url = 'http://www.dictionaryapi.com/api/v1/references/collegiate/xml/{1}?key={0}'
 downloads = './data'
 x = 0
 
-directories = ['data','parsed']
+def lookup(dictionary):
+	if os.path.isfile(dictionary):
+		with open(dictionary, 'r') as dict:
+			for word in dict:
+				evaluate(word)
+	else:
+		print ("Skipping dictionary. Could not find " + dictionary)
+
+def evaluate(word):
+	global x
+	word_file = os.path.join(downloads, word[0].upper(), word.strip() + ".xml")
+				
+	try:
+		if not os.path.isfile(word_file):
+			urllib.urlretrieve(definition_url.format(api_key, word.strip()), word_file)
+			print (str(x) + ") " + word.strip())
+	except:
+		print ("Could not download " + word)
+
+	x+=1
 
 for directory in directories:
 	for letter in alpha_numeric: 
 		if not os.path.exists(directory + '/' + letter):
 			os.makedirs(directory + '/' + letter)
 
-
-for dictionary in dictionaries:
-	if os.path.isfile(dictionary):
-		with open(dictionary, 'r') as dict:
-			for word in dict:
-				word_file = os.path.join(downloads, word[0].upper(), word.strip() + ".xml")
-				
-				try:
-					if not os.path.isfile(word_file):
-						urllib.urlretrieve(definition_url.format(api_key, word.strip()), word_file)
-						print (str(x) + ") " + word.strip())
-				except:
-					print ("Could not download " + word)
-
-				x+=1
-	else:
-		print ("Skipping dictionary. Could not find " + dictionary)
+pool = Pool(processes=5)
+results = pool.map(lookup, dictionaries)
--- python/parse.py
+++ python/parse.py
@@ -5,9 +5,13 @@ import shutil
 import xml.etree.ElementTree as ET
 import argparse
 import itertools
+import timeit
 from operator import itemgetter
 from collections import Counter, defaultdict
 from lxml import etree
+from multiprocessing import Process, Pool
+
+time_start = timeit.default_timer()
 
 alpha_numeric = ['1','2','3','4','5','6','7','8','9','0','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
 downloads = './data'
@@ -20,7 +24,6 @@ date_formats = (
 	{'exp':r'^(circa\s)(\d{1,4})$','rep':r'\g<2>', 'name':'circa XXXX', 'type':'3'},
 	{'exp':r'^(before\s)(\d{1,2})(th\scentury)$','rep':r'\g<2>00', 'name':'before XXTH century', 'type':'4'})
 
-words = []
 headers = ['id','word','parent','year','type','word_type']
 
 def getKey(item):
@@ -40,52 +43,66 @@ def get_type(date):
 
 	return 0
 
+def process_files(letter):
+	print "Starting " + letter
+	word_list = []
+	for base_folder, _, files in os.walk(os.path.join(downloads, letter.upper())):
+		for f in files:
+			word = os.path.join(base_folder, f)
+			filename, extension = os.path.splitext(word)
+
+			if (extension == '.xml'):
+				try:
+					parser = etree.XMLParser(recover=True)
+					tree = ET.parse(word, parser=parser)
+					root = tree.getroot()
+
+					if root.find('entry/def/date') is not None:
+						shutil.copyfile(word, os.path.join(parsed_folder, filename[0].upper(), f))
+					
+						for entry in root.findall('entry'):
+							for definition in entry.findall('def'):
+								if (definition.findall('date')):
+									parent = os.path.basename(f)
+									parent = os.path.splitext(parent)[0]
+									id = entry.get('id')
+									full_word = entry.find('ew').text
+									orig_date = definition.find('date').text
+									normalised_date = normalise(orig_date)
+									date_type = get_type(orig_date)
+									word_type = None
+
+									if (entry.findall('fl')):
+										word_type = entry.find('fl').text
+
+									row = [id, full_word, parent, normalised_date, date_type, word_type]
+									word_list.append(row)
+				except:
+					print "Failed on " + word
+
+	print "Finished " + letter
+	return word_list
+
 def main():
-	global words
-	for letter in alpha_numeric:
-		for base_folder, _, files in os.walk(os.path.join(downloads, letter.upper())):
-			for f in files:
-				word = os.path.join(base_folder, f)
-				filename, extension = os.path.splitext(word)
+	pool = Pool(processes=4)
+	results = pool.map(process_files, alpha_numeric)
 
-				if (extension == '.xml'):
-					try:
-						parser = etree.XMLParser(recover=True)
-						tree = ET.parse(word, parser=parser)
-						root = tree.getroot()
+	print "All processes finished"
 
-						if root.find('entry/def/date') is not None:
-							shutil.copyfile(word, os.path.join(parsed_folder, filename[0].upper(), f))
-						
-							for entry in root.findall('entry'):
-								for definition in entry.findall('def'):
-									if (definition.findall('date')):
-										parent = os.path.basename(f)
-										parent = os.path.splitext(parent)[0]
-										id = entry.get('id')
-										full_word = entry.find('ew').text
-										orig_date = definition.find('date').text
-										normalised_date = normalise(orig_date)
-										date_type = get_type(orig_date)
-										word_type = None
-
-										if (entry.findall('fl')):
-											word_type = entry.find('fl').text
-
-										row = [id, full_word, parent, normalised_date, date_type, word_type]
-										words.append(row)
-					except:
-						print "Failed on " + word
-
-		print "Finished " + letter
-
-	words = sorted([list(tup) for tup in set([tuple(row) for row in words])], key = itemgetter(3, 5, 1))
-
+	words = sorted([list(tup) for tup in set([tuple(row) for row in list(itertools.chain.from_iterable(results))])], key = itemgetter(3, 5, 1))
 	words.insert(0, headers)
 
+	print "Saving file"
+	
 	with open(os.path.join(web_folder, "words.csv"), "w") as source:
 		writer = csv.writer(source, quoting=csv.QUOTE_ALL)
 		writer.writerows(words)
+
+	print "Done"
+
+	time_end = timeit.default_timer()
+
+	print "Time elapsed: " + str(time_end-time_start) + " seconds"
 		
 if __name__ == "__main__":
 	main()
