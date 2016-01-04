dc7dc6fd2b7c9822b3f3694624214d0331031a10
struct78 <david@struct.com.au>
2016-01-05 08:08:31 +1100

    Added extra dictionaries

--- python/lookup.py
+++ python/lookup.py
@@ -3,9 +3,13 @@ import urllib
 import time
 from multiprocessing import Process, Pool
 
+# Dictionary sources
+# 20k.txt - https://github.com/first20hours/google-10000-english/
+# 355k.txt - https://github.com/dwyl/english-words
+
 api_key = os.environ['DICTIONARY_API_KEY']
 alpha_numeric = ['1','2','3','4','5','6','7','8','9','0','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
-dictionaries = ['dictionaries/new-additions.txt', 'dictionaries/english-words.txt', '/usr/share/dict/words']
+dictionaries = ['dictionaries/new-additions.txt', 'dictionaries/english-words.txt', '/usr/share/dict/words', 'dictionaries/20k.txt', 'dictionaries/355k.txt']
 directories = ['data','parsed']
 definition_url = 'http://www.dictionaryapi.com/api/v1/references/collegiate/xml/{1}?key={0}'
 downloads = './data'
@@ -37,5 +41,5 @@ for directory in directories:
 		if not os.path.exists(directory + '/' + letter):
 			os.makedirs(directory + '/' + letter)
 
-pool = Pool(processes=5)
+pool = Pool(processes=4)
 results = pool.map(lookup, dictionaries)
